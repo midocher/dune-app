95b358f64aba3fb15fdd1ef2034be13ac420a2a8
midocher <ahmedchareuf6@gmail.com>
2025-10-25 22:36:22 +0100

    Fix: Nettoyage des caractères invisibles dans les configs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <-- VÉRIFIEZ CETTE LIGNE ! ELLE EST CRUCIALE.
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
